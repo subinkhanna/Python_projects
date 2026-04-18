4818955e066a0208caa8ae6dd172bb0c3f86146b
mk <mk122592@outlook.com>
2026-04-18 14:25:39 -0700

    Added split method

--- hello.py
+++ hello.py
@@ -8,3 +8,7 @@ name = input("hello there! What is your name? ").strip().capitalize()
 # print("hello ", name)    # , also concatenates with extra space
 # print("hello, ", name, end= '. ')  with end
 print(f"hello, {name}")
+
+# Split the string into two parts
+first, last = name.split(" ")
+print(f"hello, {first}")
