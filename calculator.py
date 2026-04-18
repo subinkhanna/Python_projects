88440fa622db512412237bba6a79de4c4d837594
mk <mk122592@outlook.com>
2026-04-18 15:18:27 -0700

    Division problem with rounding within f string

--- calculator.py
+++ calculator.py
@@ -6,3 +6,9 @@ x = float(input("Enter first real number for calculation: ").strip())
 y = float(input("Enter second real number for calculation: ").strip())
 z = round((x + y), 4)
 print(f"The resulting rounding number is {z:,}")
+
+## Division problem with rounding within f string
+x = float(input("Enter first real number for division: ").strip())
+y = float(input("Enter second non-zero real number for division: ").strip())
+z = x / y
+print(f"The resulting rounding number is {z:.2f}")
