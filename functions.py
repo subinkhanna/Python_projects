04da31654dddd71f736da158bf56ed9b7e2cb229
mk <mk122592@outlook.com>
2026-04-18 15:29:40 -0700

    return value

--- functions.py
+++ functions.py
@@ -1,12 +1,16 @@
 def main():
     sayHello()
     name = input("What's your name? ").strip().title()
-    returned_value = sayHello(name)
-    print(returned_value)
-
+    #returned_value = sayHello(name)
+    #print(returned_value)
+    sayHello(name)
+    x = int(input("What integer number you wish to square? ").strip())
+    print(f"Number squared is {raiseToPower(x)}")
 
 def sayHello(to="Stranger"):
     print(f"Hello there! {to}")
 
+def raiseToPower(n):
+    return pow(n,2)
 
 main()
